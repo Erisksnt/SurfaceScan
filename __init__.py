1f225e7562f3b6756411773e183035e9a5d7f580
Erick Santos - Total Links <ericksantos@totallinks.com.br>
2026-03-09 14:48:55 -0300

    fix: structur and pyproject

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""Public package namespace for SurfaceScan.
-
-Preferred import for integrators:
-    from surfacescan import ScanConfig, parse_ports, run_scan, export_results
-"""
-
-from scanner.api import ScanConfig, export_results, parse_ports, run_scan
-
-__all__ = ["ScanConfig", "parse_ports", "run_scan", "export_results"]
